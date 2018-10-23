4d97414b53c2f27d51030ddb4cbc5891d8dc43cd
developer <mentaga.developer@gmail.com>
2018-10-23 23:14:55 +0200

    add controller actions

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdvertController extends AbstractController
 {
     /**
-     * @Route("/", name="advert")
+     * @Route("/", name="advert", methods={"GET"})
      */
     public function index()
     {
@@ -24,7 +24,7 @@ class AdvertController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="advert.new")
+     * @Route("/new", name="advert.new", methods={"GET", "POST"})
      */
     public function new()
     {
@@ -34,9 +34,29 @@ class AdvertController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="advert.show")
+     * @Route("/{id}", name="advert.show", methods={"GET"})
      */
     public function show($id)
+    {
+        return $this->render('advert/index.html.twig', [
+            'controller_name' => 'AdvertController',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="advert.show", methods={"GET", "PUT"})
+     */
+    public function edit($id)
+    {
+        return $this->render('advert/index.html.twig', [
+            'controller_name' => 'AdvertController',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="advert.delete", methods={"DELETE"})
+     */
+    public function delete($id)
     {
         return $this->render('advert/index.html.twig', [
             'controller_name' => 'AdvertController',
