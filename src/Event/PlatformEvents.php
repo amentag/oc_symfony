0767dd148b09cb5b8b928a30c82a8870b84072ac
developer <mentaga.developer@gmail.com>
2018-11-04 16:11:42 +0100

    add event name

--- src/Event/PlatformEvents.php
+++ src/Event/PlatformEvents.php
@@ -5,5 +5,6 @@ namespace App\Event;
 final class PlatformEvents
 {
     const POST_MESSAGE = 'platform.post_message';
+    const POST_OTHER_EVENT = 'platform.other_event';
     // Vos autres évènements…
 }
